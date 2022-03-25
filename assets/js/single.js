41a747612c8d5da929fcd90cd5f093028bcdd4ee
Tanya Jurecki <tljurecki@gmail.com>
2022-03-25 15:27:31 -0400

    connected a sexond html page to display open issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getRepoIssues = function(repo) {
    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,16 +10,30 @@ var getRepoIssues = function(repo) {
        if(response.ok) {
            response.json().then(function(data) {
                displayIssues(data);
+               
                if (response.headers.get("link")) {
                    displayWarning(repo);
                }
            });
        }
        else {
-           alert("There was a problem with your request!");
+           document.location.replace("./index.html");
        }
    });
 };
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    
+}
+else {
+    document.location.replace("./index.html");
+}
+};
+
 var displayIssues = function(issues) {
     if(issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
@@ -29,9 +45,8 @@ var displayIssues = function(issues) {
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href" , issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
-        issueContainerEl.appendChild(issueEl);
     }
-    var titleEl = document.createElementNS("span");
+    var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
 
     issueEl.appendChild(titleEl);
@@ -44,16 +59,19 @@ var displayIssues = function(issues) {
         typeEl.textContent = "(Issue)";
     }
     issueEl.appendChild(typeEl);
+    issueContainerEl.appendChild(issueEl);
 };
 
 var displayWarning = function(repo) {
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
     var linkEl= document.createElement("a");
-    linkEl.textContent = "See More Issues on GitHubt.com";
-    linkEl.setAttribute("href" , "https://githun.com/" + repo + "/issues");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href" , "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
 
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoIssues();
+getRepoName();
